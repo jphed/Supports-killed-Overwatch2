4d5a4ff339968c2bac67718f8bd96acf20daced5
Jorge Parra <13104@ulsachihuahua.edu.mx>
2023-10-22 17:01:42 -0700

    split into func.py and app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import tkinter as tk
+from func import calculate_stats
 
 class GameStats(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.master.title("Overwatch 2 Game Stats")
         self.damage_values_team1 = []
         self.damage_values_team2 = []
         self.healing_values_team1 = []
@@ -86,35 +88,7 @@ class GameStats(tk.Frame):
         self.healing_to_damage_ratio_display_team2.pack(pady=5)
 
     def calculate_stats(self):
-        self.damage_values_team1 = self.damage_entry_team1.get().split(",")
-        self.damage_values_team1 = [int(damage) for damage in self.damage_values_team1]
-
-        self.damage_values_team2 = self.damage_entry_team2.get().split(",")
-        self.damage_values_team2 = [int(damage) for damage in self.damage_values_team2]
-
-        self.healing_values_team1 = self.healing_entry_team1.get().split(",")
-        self.healing_values_team1 = [int(healing) for healing in self.healing_values_team1]
-
-        self.healing_values_team2 = self.healing_entry_team2.get().split(",")
-        self.healing_values_team2 = [int(healing) for healing in self.healing_values_team2]
-
-        self.total_damage_team1 = sum(self.damage_values_team1)
-        self.total_damage_team2 = sum(self.damage_values_team2)
-
-        self.total_healing_team1 = sum(self.healing_values_team1)
-        self.total_healing_team2 = sum(self.healing_values_team2)
-
-        if self.total_damage_team1 != 0:
-            self.healing_to_damage_ratio_team1 = round((self.total_healing_team2 / self.total_damage_team1) * 100, 2)
-        if self.total_damage_team2 != 0:
-            self.healing_to_damage_ratio_team2 = round((self.total_healing_team1 / self.total_damage_team2) * 100, 2)
-
-        self.total_damage_display_team1.config(text=f"{self.total_damage_team1}")
-        self.total_damage_display_team2.config(text=f"{self.total_damage_team2}")
-        self.total_healing_display_team1.config(text=f"{self.total_healing_team1}")
-        self.total_healing_display_team2.config(text=f"{self.total_healing_team2}")
-        self.healing_to_damage_ratio_display_team1.config(text=f"{self.healing_to_damage_ratio_team1}%")
-        self.healing_to_damage_ratio_display_team2.config(text=f"{self.healing_to_damage_ratio_team2}%")
+        calculate_stats(self)
 
 root = tk.Tk()
 my_gui = GameStats(root)
